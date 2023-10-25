33cea95fe42c40c70f40c70ac7c93436dfb2c0bf
Jamoben <146975543+Jamoben@users.noreply.github.com>
2023-10-25 12:57:21 +0200

    Update ListLinked.h

--- ListLinked.h
+++ ListLinked.h
@@ -12,13 +12,63 @@ class ListLinked : public List<T> {
 	    int n;
 
     public:
+
         /* Métodos de ListLinked */
+
+	ListLinked(){
+	  first = nullptr;
+          n = 0;
+	}
+
+	~ListLinked(){
+	  Node<T>* aux = first;
+	  while(aux != nullptr){
+	    Node<T>* next = aux->next;
+	    delete next;
+	    aux = next;
+	  }
+	}
+
+	T operator[](int pos){
+	  if(pos < 0 || pos >=n ){
+	    throw out_of_range("Posicion invalida");
+	  }
+			
+	  Node<T>* aux = first;
+	    for(int i = 0;i < pos;i++){
+	      aux = aux->next;
+	    }
+	  return aux->data;
+	}
+
+	friend std::ostream& operator<<(std::ostream &out, const ListLinked<T> &list){
+	  out << "List => [";
+          Node<T>* aux = list.first;
+	  int siguiente = 0;
+	  while(aux != nullptr){
+	    out << "\n\t" << aux->data;
+	    aux = aux->next;
+	    siguiente = 1;
+	  }
+	  if(siguiente){
+            out << "\n";
+          }
+	  out << "]";
+          return out;
+ 	}
+
         /* Métodos de List */
+
         void insert(int pos, T e) override{
           if(pos < 0 || pos > size()){
               throw out_of_range("Posición no válida");
           }else{
-            arr[pos] = e;
+            Node<T>* aux = new Node<T>(e);
+	    aux = first;
+	    for(int i = 1 ; i < pos ; i++){
+              aux = aux->next;
+            }
+	    n += 1;
           }
         }
 
@@ -34,8 +84,15 @@ class ListLinked : public List<T> {
           if(pos < 0 || pos > size()){
               throw out_of_range("Posición no válida");
           }else{
-            T elemento = arr[pos];
-            arr[pos] = NULL;
+	    Node<T>* aux = first;
+	    for(int i = 1; i < pos; i++){
+		aux = aux->next;
+	    }
+            Node<T>* borrar = aux->next;
+            aux->next = borrar->next;
+	    T elemento = borrar->data;
+	    delete borrar;
+	    n--;
             return elemento;
           }
         }
@@ -44,13 +101,18 @@ class ListLinked : public List<T> {
           if(pos < 0 || pos > size()){
               throw out_of_range("Posición no válida");
           }else{ 
-            return arr[pos];
+             Node<T>* aux = first;
+	     for (int i = 0; i < pos; i++) {
+	       aux = aux->next;
+	     }
+	  return aux->data;
           }
         }  
 
         int search(T e) override{
+	  Node<T>* aux = first;
           for(int i = 0; i < size(); i++){
-            if(arr[i] == e)
+            if(aux->data == e)
               return i;
           }
           return -1;
